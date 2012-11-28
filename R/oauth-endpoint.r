1171e608c995be57d5d380ae460f6688731724be
Thomas Cabrol <thomas.cabrol@gmail.com>
2012-11-28 14:32:23 +0100

    Using query param

--- R/oauth-endpoint.r
+++ R/oauth-endpoint.r
@@ -20,11 +20,11 @@ oauth_endpoint <- function(request = NULL, authorize, access, base_url = NULL, s
   if (!is.null(base_url)) {
     path <- parse_url(base_url)$path
     list(
-      request = modify_url(base_url, path = file.path(path, request), scope = scope),
+      request = modify_url(base_url, path = file.path(path, request), query = scope),
       authorize = modify_url(base_url, path = file.path(path, authorize)),
       access = modify_url(base_url, path = file.path(path, access))
     )
   } else {
-    list(request = request, authorize = authorize, access = access, scope = scope)
+    list(request = request, authorize = authorize, access = access, query = scope)
   }
 }
